b5f45ca0e67b3eb04d0f341c20d526d31b6a32c5
Allison Nelson <nelsonam@appstate.edu>
2012-12-27 10:34:38 -0500

    optimized the loops
    we don't need both i and j (1-7), since i==j just use i both places

--- traveling_santa.py
+++ traveling_santa.py
@@ -12,20 +12,17 @@ G.add_weighted_edges_from([(1,7,15),(1,3,6),(1,5,7),(1,6,6),(1,2,4),(2,4,10),(3,
 
 distances=[]
 #go through each type of cycle we can have (1-7)
-for i in range(1,8):
-    for j in range(1,8):
-        #start and end at the same place
-        if(i==j):
-            #cutoff is to make sure we get out to the ham. cycle
-            for path in nx.all_simple_paths(G, source=i, target=j, cutoff=len(G)):
-                #we only want the longest ones
-                if(len(path)==len(G)+1):
-                    distance=0
-                    #add up the weights for each edge
-                    for i in range(0,7):
-                        distance=distance+G[path[i]][path[i+1]]['weight']
-                    #add these to a list
-                    distances.append((distance,path))
+for i in range(1,8): #1-8 because range is not inclusive
+    #cutoff is to make sure we get out to the ham. cycle
+    for path in nx.all_simple_paths(G, source=i, target=i cutoff=len(G)):
+        #we only want the longest ones
+        if(len(path)==len(G)+1):
+            distance=0
+            #add up the weights for each edge
+            for i in range(0,7):
+                distance=distance+G[path[i]][path[i+1]]['weight']
+            #add these to a list
+            distances.append((distance,path))
 
 #defaults
 min_dist=distances[0];
